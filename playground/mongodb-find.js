2b6f2aa191b105f7b2f3d087a4f1d4d6db5f9490
alvesalex <alex@alexalves.net>
2019-02-25 20:10:43 -0300

    Init commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,35 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017', (err, client) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+
+    const db = client.db('TodoApp');
+
+    // db.collection('Todos').find({
+    //     _id: new ObjectID('5c74709e63ba693676f754a1')
+    // }).toArray().then((docs) => {
+    //     console.log('Todos');
+    //     console.log(JSON.stringify(docs, undefined, 4));
+    // }, (err) => {
+    //     console.log('Unable to fetch todos', err);
+    // });
+
+    // db.collection('Todos').find().count().then((count) => {
+    //     console.log(`Todos count: ${count}`);
+    // }, (err) => {
+    //     console.log('Unable to fetch todos', err);
+    // });
+
+    db.collection('Users').find({name: 'Alex Alves'}).toArray().then((users) => {
+        console.log('Users');
+        console.log(JSON.stringify(users, undefined, 4));
+    }, (err) => {
+        console.log('Unable to fetch users', err);
+    });
+
+    // client.close();
+});
